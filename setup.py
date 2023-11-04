6c1615249e4bf870e81f41179205b32a96c5f97e
Jack Spyvee <jackspyvee@comcast.net>
2023-11-03 23:32:21 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="RexTools",
+    version="0.1",
+    url="https://github.com/drfragger/RexTools",
+    install_requires=[
+        "opentrons",
+        "pandas",
+        "openpyxl"
+    ],
+
+)
