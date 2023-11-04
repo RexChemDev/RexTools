4349dbfe2fad6bdcce592a657207baa279ea7fb5
Jack Spyvee <jackspyvee@comcast.net>
2023-11-03 23:53:04 -0400

    Restructuring

--- setup.py
+++ setup.py
@@ -1,9 +1,6 @@
 from setuptools import setup
 
 setup(
-    name="RexTools",
-    version="0.1",
-    url="https://github.com/drfragger/RexTools",
     install_requires=[
         "opentrons",
         "pandas",
